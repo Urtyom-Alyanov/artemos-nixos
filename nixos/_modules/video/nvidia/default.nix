86716732f8a1373a5eb9d18d9430a37be6f4976d
Urtyom-Alyanov <urtyomalyanov@gmail.com>
2026-06-06 15:30:17 +0300

    modules(fix): nvidia.enabled is readonly

--- nixos/_modules/video/nvidia/default.nix
+++ nixos/_modules/video/nvidia/default.nix
@@ -48,7 +48,6 @@ in {
 
     hardware = {
       nvidia = {
-        enabled = true;
         open = moduleConfig.openDrivers;
         powerManagement.enable = true;
         modesetting.enable = true;
